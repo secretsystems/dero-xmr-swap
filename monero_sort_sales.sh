66ef148784f96d028d23a90f7ad1f9df9d5d3897
secretnamebasis <86203717+secretnamebasis@users.noreply.github.com>
2023-04-22 05:56:57 -0600

    Update monero_sort_sales.sh

--- monero_sort_sales.sh
+++ monero_sort_sales.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-echo "Scanning: $txid"
+
 # Check  if in db
 if [ ! -s "$monero_pong_db" ] ; then
 		echo "No tx history..."
 fi
 
+echo "Scanning: $txid"
 if [[ "$already_processed" =~ "$time" ]] ; then 
 			echo "Already processed, skipping"
 			continue
 fi
+
 # Check if pay_id
 if [[ "$payment_id" == "$xmr_payment_id" ]]; then
     source ./monero_convert_dero.sh
