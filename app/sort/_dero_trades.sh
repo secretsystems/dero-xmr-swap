16c7b780e62582fbd2d9c27e62221164a1b55fa7
secretnamebasis <secretnamebasis@keemail.me>
2023-11-27 21:53:15 -0700

    feat: authenticate seperately

--- app/sort/_dero_trades.sh
+++ app/sort/_dero_trades.sh
@@ -22,7 +22,7 @@ if [[ "$dst_port" == "$xmr_for_dero_port" && "$amount" -gt "2" ]]; then
     if [[ "$comment" ]]; then
 
         # Send the request to the Monero daemon's JSON-RPC interface
-        response=$(curl -u $user:$pass --digest --silent --data-binary '{"jsonrpc":"2.0","id":"0","method":"validate_address","params":{"address":"'"$comment"'","any_net_type":true,"allow_openalias":true}}' -H 'content-type: application/json;' http://$monero_ip:$monero_port/json_rpc)
+        response=$(curl -u $user:$monero_pass --digest --silent --data-binary '{"jsonrpc":"2.0","id":"0","method":"validate_address","params":{"address":"'"$comment"'","any_net_type":true,"allow_openalias":true}}' -H 'content-type: application/json;' http://$monero_ip:$monero_port/json_rpc)
 
         # Check if the Monero address is valid
         valid=$(echo "$response" | jq -r '.result.valid')
--- app/transfer/_xmr.sh
+++ app/transfer/_xmr.sh
@@ -17,7 +17,7 @@ payload=$(jq -n --arg addr "$addr" --arg amnt "$amnt" \
     }}')
 
 # Send the transfer request using cURL
-response=$(curl -u $user:$pass --digest -s -X POST -H 'Content-type: application/json' -d "$payload" http://$monero_ip:$monero_port/json_rpc)
+response=$(curl -u $user:$monero_pass --digest -s -X POST -H 'Content-type: application/json' -d "$payload" http://$monero_ip:$monero_port/json_rpc)
 
 # Check if the transfer was successful
 txid=$(echo "$response" | jq -r '.result.tx_hash')
