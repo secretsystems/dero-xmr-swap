c92325191263eaaddeed5b63ec86ea9cea3b05f5
secretnamebasis <secretnamebasis@keemail.me>
2023-12-19 06:41:59 -0700

    clean: clairfy outputs

--- app/convert/_dero_to_xmr.sh
+++ app/convert/_dero_to_xmr.sh
@@ -9,23 +9,24 @@ echo "SERVICE MSG: dero is trading at $get_usdt_dero_quote"
 
 # Calculate USDT value of the transfer amount in DERO
 amount_usdt=$(echo "$get_usdt_dero_quote * $amount * 0.00001" | bc -l)
-echo "SERVICE MSG: the dero $amount is trading at $amount_usdt"
+formatted=$(echo "$amount * 0.00001" | bc -l)
+echo "SERVICE MSG: the dero amount $formatted is trading for $amount_usdt USDT"
 
 # Get bid for USDT-XMR pair from TradeOgre API
 get_usdt_xmr_bid=$(curl -s -X GET https://tradeogre.com/api/v1/ticker/xmr-usdt | jq -r '. | (.bid)')
-echo "SERVICE MSG: dero is trading at $get_usdt_xmr_bid"
+echo "SERVICE MSG: xmr is trading at $get_usdt_xmr_bid USDT"
 
 # Calculate amount in XMR based on the calculated amount in USDT
 amount_xmr=$(bc <<< "scale=12;$amount_usdt / $get_usdt_xmr_bid")
-echo "SERVICE MSG: the xmr $amount is worth at $amount_xmr"
+echo "SERVICE MSG: the dero amount $formatted is worth $amount_xmr xmr"
 
 # Calculate a % of XMR as fee
 fee_xmr=$(echo "$amount_xmr * 0.01" | bc)
-echo "SERVICE MSG: the xmr $amount_xmr is worth at $fee_xmr"
+echo "SERVICE MSG: the xmr $amount_xmr fee is $fee_xmr xmr"
 
 # Calculate a % of XMR for the trade after fee deduction
 trade_xmr=$(echo "$amount_xmr * 0.99" | bc)
-echo "SERVICE MSG: the xmr $amount_xmr is worth at $trade_xmr"
+echo "SERVICE MSG: the xmr $amount_xmr to be sent is $trade_xmr"
 
 # Remove decimals and leading zeroes
 amnt=$(echo "$trade_xmr" | awk '{ gsub(/^0+|\./,""); print}')
